2256b19e93806e24e259ecd005763359780f496e
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-09-05 10:49:04 +0200

    inital commit
    useFetch

---
+++ custom-hook/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { useFetch } from './components/Custom hooks/useFetch'
+
+function App() {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
+
+  return (
+    <>
+    <h1>Custom hooks</h1>
+    <section>
+      <h3>To do API; useFetch hook</h3>
+      {data &&
+      data.map((item) => {
+        return <p key={item.id}>{item.title}</p>
+      })}
+      </section>
+    </>
+  )
+}
+
+export default App
