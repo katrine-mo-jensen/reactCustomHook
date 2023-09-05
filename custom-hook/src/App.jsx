540c9015e818abf26eb487d4675d8b30209799e5
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-09-05 12:24:42 +0200

    Kaspers metode

--- custom-hook/src/App.jsx
+++ custom-hook/src/App.jsx
@@ -1,18 +1,27 @@
 
 import './App.css'
-import { useFetch } from './components/Custom hooks/useFetch'
+// import { useFetch } from './components/Custom hooks/useFetch'
+import { useFeetch } from './components/Custom hooks/codealongUseFetch'
 
 function App() {
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
-
+  // const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
+  const url = "https://jsonplaceholder.typicode.com/todos"
+  const {data} = useFeetch(url)
+  console.log({data})
   return (
     <>
     <h1>Custom hooks</h1>
     <section>
-      <h3>To do API; useFetch custom hook</h3>
+{/*       <h3>To do API; useFetch custom hook</h3>
       {data &&
       data.map((item) => {
         return <p key={item.id}>{item.title}</p>
+      })} */}
+      <h3>Kaspers metode</h3>
+      {data?.map((item, index) => {
+        return(
+          <p key={index}>{item.title}</p>
+        )
       })}
       </section>
     </>
